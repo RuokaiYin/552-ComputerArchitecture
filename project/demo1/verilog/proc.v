22ece966aa88cfad7d99c85648b2c6ff9874f5f9
Ruokai Yin <ryin25@wisc.edu>
2020-03-31 22:57:02 -0500

    Add single cycle implementations for phase1, all tests passed

--- project/demo1/verilog/proc.v
+++ project/demo1/verilog/proc.v
@@ -21,8 +21,75 @@ module proc (/*AUTOARG*/
    // As desribed in the homeworks, use the err signal to trap corner
    // cases that you think are illegal in your statemachines
    
-   
+   wire err_1, err_2;
    /* your code here -- should include instantiations of fetch, decode, execute, mem and wb modules */
-   
-endmodule // proc
-// DUMMY LINE FOR REV CONTROL :0:
+   wire  Halt, halt_back; 
+   wire [15:0] PC_Back,PC_Next, No_Branch, instr; 
+	fetch fet(
+        // system inputs
+	.clk(clk), .rst(rst), 
+	// inputs from Decode
+	.PC_Back(PC_Back), .Halt(Halt),  
+	// Outputs to Decode
+	.No_Branch(No_Branch), .instr(instr), .halt_back(halt_back),
+        // Output to WB
+        .PC_Next(PC_Next) // .PC_curr(PC_curr)
+	);
+
+   wire neg, zero,Mem_read,Mem_wrt;
+   wire [15:0] WB, result;
+
+   wire [1:0] Op_ext, WB_sel,Alu_src;
+   wire [2:0] Alu_result;
+   wire [4:0] Alu_op;
+   wire [15:0] data1,data2,extend;
+	decode dec(
+        // IN from Fetch
+	.instr(instr), .No_Branch(No_Branch), .halt_back(halt_back),
+        // IN from Exec
+        .result(result), .neg(neg), .zero(zero),
+        // IN from WB
+        .WB(WB),
+        // Global In
+        .clk(clk), .rst(rst),
+	// Out Control Logic
+	.Halt(Halt),.WB_sel(WB_sel),.Alu_src(Alu_src),.Alu_result(Alu_result),.Alu_op(Alu_op),.Mem_read(Mem_read),.Mem_wrt(Mem_wrt),
+        // Out to Exec
+	.data1(data1),.data2(data2),.extend(extend), .Op_ext(Op_ext),
+        // Out to Fetch
+        .PC_back(PC_Back),
+        // Global out
+        .err(err_1));
+    wire [15:0] Cout, SLBI, BTR;
+	execute exe(
+	// Inputs from Decode
+	.data1(data1), .data2(data2), .extend(extend), .Alu_Src(Alu_src), .Alu_op(Alu_op), .Op_ext(Op_ext), 
+	// Outputs to Decode/Memory
+	.result(result), .zero(zero), .neg(neg), .Cout(Cout), .SLBI(SLBI), .BTR(BTR), .err(err_2)
+	);
+   wire [15:0] data_mem, data_exe;
+	memory mem(
+	// system inputs
+	.clk(clk), .rst(rst),
+	// input from Decode
+        .data2(data2), .Mem_read(Mem_read), .Mem_wrt(Mem_wrt), .Halt(Halt), .Alu_result(Alu_result),
+	// inputs from Execute
+	.result(result), .zero(zero), .neg(neg), .BTR(BTR), .SLBI(SLBI), .Cout(Cout),  
+	// outputs to WB
+	.data_mem(data_mem), .data_exe(data_exe)
+	);
+
+	wb wrib(
+        // IN from Fetch
+        .PC_Next(PC_Next),
+        // IN from Decode
+        .extend(extend), .WB_sel(WB_sel),
+        // IN from Mem
+        .data_mem(data_mem), .data_exe(data_exe),
+        // Out to Decode
+        .WB(WB));
+   	
+	assign err = 1'b0;
+		
+endmodule 
+
--- project/demo1/verilog/proc_hier_bench.v
+++ project/demo1/verilog/proc_hier_bench.v
@@ -124,31 +124,31 @@ module proc_hier_bench();
    // Edit the example below. You must change the signal
    // names on the right hand side
     
-   assign PC = DUT.p0.fetch0.pcCurrent;
-   assign Inst = DUT.p0.fetch0.instr;
+   assign PC = DUT.p0.fet.PC_curr;
+   assign Inst = DUT.p0.fet.instr;
    
-   assign RegWrite = DUT.p0.decode0.regFile0.write;
+   assign RegWrite = DUT.p0.dec.Reg_wrt;
    // Is register being written, one bit signal (1 means yes, 0 means no)
    
-   assign WriteRegister = DUT.p0.decode0.regFile0.writeregsel;
+   assign WriteRegister = DUT.p0.dec.target_reg;
    // The name of the register being written to. (3 bit signal)
 
-   assign WriteData = DUT.p0.decode0.regFile0.writedata;
+   assign WriteData = DUT.p0.dec.WB;
    // Data being written to the register. (16 bits)
    
-   assign MemRead =  DUT.p0.memory0.memRead;
+   assign MemRead =  DUT.p0.mem.Mem_read;
    // Is memory being read, one bit signal (1 means yes, 0 means no)
    
-   assign MemWrite = (DUT.p0.memory0.memReadorWrite & DUT.p0.memory0.memWrite);
+   assign MemWrite = (DUT.p0.mem.Mem_wrt);
    // Is memory being written to (1 bit signal)
    
-   assign MemAddress = DUT.p0.memory0.aluResult;
+   assign MemAddress = DUT.p0.mem.data_exe;
    // Address to access memory with (for both reads and writes to memory, 16 bits)
    
-   assign MemData = DUT.p0.memory0.writeData;
+   assign MemData = DUT.p0.mem.data2;
    // Data to be written to memory for memory writes (16 bits)
    
-   assign Halt = DUT.p0.memory0.halt;
+   assign Halt = DUT.p0.dec.Halt;
    // Is processor halted (1 bit signal)
    
    /* Add anything else you want here */
