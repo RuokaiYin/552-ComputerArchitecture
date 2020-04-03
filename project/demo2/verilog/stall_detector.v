695ec98305f8ef09ef2055fe779fc94d577e3b3f
Ruokai Yin <ryin25@wisc.edu>
2020-04-03 13:20:13 -0500

    update stall signal condition

--- project/demo2/verilog/stall_detector.v
+++ project/demo2/verilog/stall_detector.v
@@ -1,13 +1,13 @@
 module stall_detector (instr_reg, Reg_wrt_reg_ID, target_reg_ID, Reg_wrt_reg_EX, target_reg_EX, STALL);
 input [15:0] instr_reg;
 input Reg_wrt_reg_ID, Reg_wrt_reg_EX;
-// input [1:0] Rt_valid_reg;
+// input [1:0] Alu_src_reg;
 input [2:0] target_reg_ID, target_reg_EX; 
 // input [4:0] Alu_op_reg; 
 output STALL;
 
-wire Rt_valid; // determine whther Rt is valid
-assign Rt_valid = ((instr_reg[15:14] === 2'b11) & (instr_reg[15:11] !== 5'b11001)) | ((instr_reg[15:11] === 5'b10000) | (instr_reg[15:11] == 5'b10011));
+wire Rt_valid;
+assign Rt_valid = ((instr_reg[15:14] === 2'b11) & (instr_reg[15:11] !== 5'b11001) & (instr_reg[15:11] !== 5'b11000)) | ((instr_reg[15:11] === 5'b10000) | (instr_reg[15:11] == 5'b10011));
 
 wire IDEX_wrt;
 wire IDEX_Rs;
@@ -28,7 +28,5 @@ assign EXM_Rt = (instr_reg[7:5] === target_reg_EX) & (Rt_valid);
 assign EXM_stall = EXM_wrt & (EXM_Rs | EXM_Rt);
 
 assign STALL = IDEX_stall | EXM_stall; 
-
-
 
 endmodule
