5c50691aa3f9edc75078bb1b6f809c13c301f9c6
Ruokai Yin <ryin25@wisc.edu>
2020-04-27 21:42:59 -0500

    debug

--- project/demo2/verilog/memory.v
+++ project/demo2/verilog/memory.v
@@ -28,9 +28,9 @@ module memory (
    output [15:0] data_mem, data_exe;
    // output err;
 
-//    wire err_reg_dummy1, Mem_read_q, Mem_wrt_q;
-//    reg_16 #(.SIZE(1)) memwrt_reg(.readData(Mem_wrt_q), .err(err_reg_dummy1), .clk(clk), .rst(rst), .writeData(Mem_wrt), .writeEn(~Stall_dmem));
-//    reg_16 #(.SIZE(1)) mwmread_reg(.readData(Mem_read_q), .err(err_reg_dummy1), .clk(clk), .rst(rst), .writeData(Mem_read), .writeEn(~Stall_dmem));
+   wire err_reg_dummy1, Mem_read_q, Mem_wrt_q;
+   reg_16 #(.SIZE(1)) memwrt_reg(.readData(Mem_wrt_q), .err(err_reg_dummy1), .clk(clk), .rst(rst), .writeData(Mem_wrt), .writeEn(~Stall_dmem));
+   reg_16 #(.SIZE(1)) mwmread_reg(.readData(Mem_read_q), .err(err_reg_dummy1), .clk(clk), .rst(rst), .writeData(Mem_read), .writeEn(~Stall_dmem));
 
    // use a 8-1 mux to choose desired result from Execute stage
    wire zero_or_neg;
@@ -40,8 +40,8 @@ module memory (
                       (Alu_result[1] ? (Alu_result[0] ? {15'b0, neg} : {15'b0, zero}) : (Alu_result[0] ? Cout : result));
    
    wire Done, CacheHit;
-   assign Mem_wrt_tmp =Mem_wrt;
-   assign Mem_read_tmp = Mem_read;
+   assign Mem_wrt_tmp = Stall_dmem ? Mem_wrt_q : Mem_wrt;
+   assign Mem_read_tmp = Stall_dmem ? Mem_read_q : Mem_read;
    // create a data memory
    //memory2c_align mem_data(.data_out(data_mem), .data_in(data2), .addr(data_exe), .enable(~Halt & (Mem_wrt | Mem_read)), .wr(Mem_wrt), .createdump(Halt), .clk(clk), .rst(rst), .err(err));
    stallmem mem_data(.DataOut(data_mem), .Done(Done), .Stall(Stall_dmem), .CacheHit(CacheHit), .err(err), .Addr(data_exe), .DataIn(data2), .Rd(~Halt & (Mem_wrt_tmp | Mem_read_tmp)), .Wr(Mem_wrt_tmp), .createdump(Halt), .clk(clk), .rst(rst));
