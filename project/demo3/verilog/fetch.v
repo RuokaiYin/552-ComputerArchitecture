45f701c0adbb1a44e40ecaa655d0551c09319304
Ruokai Yin <ryin25@wisc.edu>
2020-05-02 22:43:40 -0500

    Revert "another try to improve cpi"
    This reverts commit 798f3e77b9d577fb64494e26be5d716721221b20.

--- project/demo3/verilog/fetch.v
+++ project/demo3/verilog/fetch.v
@@ -24,7 +24,7 @@ module fetch (
 
    wire Stall_imem_nextcycle,Stall_dmem_nextcycle, Branch_stall_q, Stall_imem_q, Stall_dmem_q, dBranch_stall_q, dStall_imem_q; 
    // use a 16-bit register to store the PC value
-   wire [15:0] PC_curr, PC_wb, PC_wb_plus_stall, PC_Back_q, PC_Back_with_stall, PC_branch_opt;
+   wire [15:0] PC_curr, PC_wb, PC_wb_plus_stall, PC_Back_q, PC_Back_with_stall;
    wire err_reg, err_reg_dummy1,err_reg_dummy2,err_reg_dummy3;
    wire Done, CacheHit;
    wire branch_stall_reg_clr, branch_stall_reg_en;
@@ -46,18 +46,18 @@ module fetch (
    // reg_16 #(.SIZE(16)) pc_back_reg_d(.readData(dPC_Back_q), .err(err_reg_dummy3), .clk(clk), .rst(rst), .writeData(PC_Back), .writeEn(Branch_stall));
 
    assign PC_Back_with_stall = (Stall_imem_nextcycle|Stall_dmem_nextcycle) ? PC_Back_q : PC_Back;
-   //assign branch_with_stall = (Stall_imem_nextcycle|Stall_dmem_nextcycle) ? Branch_stall_q : Branch_stall;
-   assign branch_with_stall = (Stall_imem_nextcycle) ? Branch_stall_q : Branch_stall;
+   assign branch_with_stall = (Stall_imem_nextcycle|Stall_dmem_nextcycle) ? Branch_stall_q : Branch_stall;
 
     // a mux to choose from normal pc+2 or pc_back
    assign PC_wb = (branch_with_stall) ? PC_Back_with_stall : PC_Next;
    assign PC_wb_plus_stall = Stall_imem|Stall_dmem ? PC_curr: PC_wb;
    reg_16 pc_reg (.readData(PC_curr), .err(err_reg), .clk(clk), .rst(rst), .writeData(PC_wb_plus_stall), .writeEn(~STALL));
-   assign PC_branch_opt = (Stall_dmem_nextcycle & Branch_stall_q) ? PC_Back_q : PC_curr;
+   
   
    // add current PC value by 2 
    wire C_out;
-   cla_16b add_2 (.A(PC_branch_opt), .B(16'b0000_0000_0000_0010), .C_in(1'b0), .S(PC_Next), .C_out(C_out));
+
+   cla_16b add_2 (.A(PC_curr), .B(16'b0000_0000_0000_0010), .C_in(1'b0), .S(PC_Next), .C_out(C_out));
 
    // use a dff to store HALT signal
    wire halt_q;
@@ -68,7 +68,7 @@ module fetch (
    // instruction memory
    // memory2c_align instr_mem(.data_out(instr), .data_in(16'b0), .addr(PC_curr), .enable(~halt_q), .wr(1'b0), .createdump(halt_q), .clk(clk), .rst(rst), .err(err));
    // stallmem instr_mem(.DataOut(instr), .Done(Done), .Stall(stall_temp), .CacheHit(CacheHit), .err(err), .Addr(PC_curr), .DataIn(16'bx), .Rd(~halt_q), .Wr(1'b0), .createdump(halt_q), .clk(clk), .rst(rst));
-   mem_system #(.memtype(0))mem_instr(.DataOut(instr), .Done(Done), .Stall(stall_temp), .CacheHit(CacheHit), .err(err), .Addr(PC_branch_opt), .DataIn(16'bx), .Rd(~halt_q&~Branch_stall), .Wr(1'b0), .createdump(halt_q), .clk(clk), .rst(rst));
+   mem_system #(.memtype(0))mem_instr(.DataOut(instr), .Done(Done), .Stall(stall_temp), .CacheHit(CacheHit), .err(err), .Addr(PC_curr), .DataIn(16'bx), .Rd(~halt_q&~Branch_stall), .Wr(1'b0), .createdump(halt_q), .clk(clk), .rst(rst));
    
    assign halt_back = halt_q;
    assign Stall_imem = stall_temp & ~Done;
