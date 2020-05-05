dacc72bd7d22979c9d5cff8b17a71d09dd0c4850
Ruokai Yin <ryin25@wisc.edu>
2020-05-05 00:49:31 -0500

    try on dynamic predict

--- project/demo3/verilog/fetch.v
+++ project/demo3/verilog/fetch.v
@@ -8,23 +8,23 @@ module fetch (
 	// system inputs
 	clk, rst, Stall_dmem,
 	// inputs from Decode
-	PC_Back, Halt, STALL, Branch_stall, siic, rti, epc, 
+	PC_Back, Halt, STALL, Branch_stall, siic, rti, epc, branch_taken, Branch, 
 	// Outputs to Decode
 	PC_Next, No_Branch, instr, halt_back, // PC_curr
 	//system output
-	err, Stall_imem, branch_with_stall
+	err, Stall_imem, branch_with_stall, branch_taken_dy
 	); 
 
    // TODO: Your code here
-   input clk, rst, Halt, STALL, Branch_stall, Stall_dmem, siic, rti; 
+   input clk, rst, Halt, STALL, Branch_stall, Stall_dmem, siic, rti, branch_taken, Branch; 
    input [15:0] PC_Back, epc;
 
    output [15:0] PC_Next, No_Branch, instr; 
-   output halt_back, err, Stall_imem, branch_with_stall;
+   output halt_back, err, Stall_imem, branch_with_stall, branch_taken_dy;
 
-   wire Stall_imem_nextcycle,Stall_dmem_nextcycle, Branch_stall_q, Stall_imem_q, Stall_dmem_q; 
+   wire Stall_imem_nextcycle,Stall_dmem_nextcycle, Branch_stall_q, Stall_imem_q, Stall_dmem_q, branch_prefetch; 
    // use a 16-bit register to store the PC value
-   wire [15:0] PC_curr, PC_wb, PC_wb_plus_stall, PC_Back_q, PC_Back_with_stall, PC_with_exception;
+   wire [15:0] PC_curr, PC_wb, PC_wb_plus_stall, PC_Back_q, PC_Back_with_stall, PC_with_exception, pc_branch_taken, pc_dyn_latch, pc_final;
    wire err_reg, err_reg_dummy1,err_reg_dummy2,err_reg_dummy3;
    wire Done, CacheHit;
    wire branch_stall_reg_clr, branch_stall_reg_en;
@@ -50,7 +50,10 @@ module fetch (
    assign PC_wb = (branch_with_stall) ? PC_Back_with_stall : PC_Next;
    assign PC_wb_plus_stall = Stall_imem|Stall_dmem ? PC_curr: PC_wb;
    assign PC_with_exception = siic ? (16'b0000_0000_0000_0010) : (rti ? epc : PC_wb_plus_stall);
-   reg_16 pc_reg (.readData(PC_curr), .err(err_reg), .clk(clk), .rst(rst), .writeData(PC_with_exception), .writeEn(~STALL));
+
+   assign pc_final = ((Branch)&(~branch_taken&branch_taken_dy)) ? pc_dyn_latch : ((branch_taken_dy&branch_prefetch)? pc_branch_taken : PC_with_exception);
+
+   reg_16 pc_reg (.readData(PC_curr), .err(err_reg), .clk(clk), .rst(rst), .writeData(pc_final), .writeEn(~STALL));
    
   
    // add current PC value by 2 
@@ -69,6 +72,14 @@ module fetch (
    // stallmem instr_mem(.DataOut(instr), .Done(Done), .Stall(stall_temp), .CacheHit(CacheHit), .err(err), .Addr(PC_curr), .DataIn(16'bx), .Rd(~halt_q), .Wr(1'b0), .createdump(halt_q), .clk(clk), .rst(rst));
    mem_system #(.memtype(0))mem_instr(.DataOut(instr), .Done(Done), .Stall(stall_temp), .CacheHit(CacheHit), .err(err), .Addr(PC_curr), .DataIn(16'bx), .Rd(~halt_q&~Branch_stall), .Wr(1'b0), .createdump(halt_q), .clk(clk), .rst(rst));
    
+   assign branch_prefetch = (instr[14]&instr[13]) ? (instr[15] ? 1'b0 : 1'b1) : 1'b0;
+   cla_16b branch_adder(.A({{8{instr[7]}},instr[7:0]}), .B(No_Branch), .C_in(1'b0), .S(pc_branch_taken), .C_out(C_out));
+   reg_16 #(.SIZE(1)) dynamic_pc_latch (.readData(branch_taken_dy), .err(err_reg), .clk(clk), .rst(rst), .writeData(PC_wb_plus_stall), .writeEn(branch_taken_dy&branch_prefetch));
+   
+   
+   reg_16 #(.SIZE(16)) dynamic_predict (.readData(pc_dyn_latch), .err(err_reg), .clk(clk), .rst(rst), .writeData(branch_taken), .writeEn(Branch));
+
+
    assign halt_back = halt_q;
    assign Stall_imem = stall_temp & ~Done;
 
--- project/demo3/verilog/proc.v
+++ project/demo3/verilog/proc.v
@@ -24,7 +24,7 @@ module proc (/*AUTOARG*/
    
    /* your code here -- should include instantiations of fetch, decode, execute, mem and wb modules */
    wire err_1, err_2;
-   wire err_mem_fetch, Stall_imem, Stall_dmem, branch_with_stall, siic, rti;
+   wire err_mem_fetch, Stall_imem, Stall_dmem, branch_with_stall, siic, rti, branch_taken, Branch, branch_taken_dy;
 
 
    // test wires
@@ -39,17 +39,21 @@ module proc (/*AUTOARG*/
         // system inputs
 	.clk(clk), .rst(rst), .Stall_dmem(Stall_dmem),
 	// inputs from Decode
-	.PC_Back(PC_Back), .Halt(Halt), .STALL(STALL), .Branch_stall(Branch_stall), .siic(siic), .rti(rti), .epc(PC_recover),
+	.PC_Back(PC_Back), .Halt(Halt), .STALL(STALL), .Branch_stall(Branch_stall), .siic(siic), .rti(rti), .epc(PC_recover), .branch_taken(branch_taken), .Branch(Branch), 
 	// Outputs to Decode
 	.No_Branch(No_Branch), .instr(instr), .halt_back(halt_back),
         // Output to WB
         .PC_Next(PC_Next), // .PC_curr(PC_curr)
-	.err(err_mem_fetch), .Stall_imem(Stall_imem), .branch_with_stall(branch_with_stall));
+	.err(err_mem_fetch), .Stall_imem(Stall_imem), .branch_with_stall(branch_with_stall), .branch_taken_dy(branch_taken_dy));
 
    
-   wire [15:0] instr_withNOP;
+   wire [15:0] instr_withNOP, instr_dynamic, instr_dynamic_withNOP;
+
    // add a mux to choose from normal instr or NOP on stall of Branch
    assign instr_withNOP = (Branch_stall|Stall_imem|branch_with_stall) ? 16'b00001_xxxxxxxxxxx : instr;
+   assign instr_dynamic_withNOP = (Branch & ~branch_taken) ? 16'b00001_xxxxxxxxxxx : instr;
+
+   assign instr_dynamic = (Branch & branch_taken_dy) ? instr_dynamic_withNOP : instr_withNOP;
 
 
    // IF/ID Pip Reg
@@ -146,7 +150,7 @@ module proc (/*AUTOARG*/
         // Out to Fetch
         .PC_back(PC_Back), .Branch_stall(Branch_stall),
         // Global out
-        .err(err_1), .fwd(fwd_possible), .siic(siic), .rti(rti));
+        .err(err_1), .fwd(fwd_possible), .siic(siic), .rti(rti), .branch_taken(branch_taken), .Branch(Branch));
 
     // ID/EX pip reg
    wire IDEX_en,IDEX_err;
